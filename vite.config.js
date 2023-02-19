793f41f35f620e9925e7680e8c3a5778d27274af
davidleston <david.leston@gmail.com>
2023-02-18 22:28:01 -0800

    Sourcemaps now included. Type declaration file now named consistently with other files.

--- vite.config.js
+++ vite.config.js
@@ -13,18 +13,7 @@ export default defineConfig({
       // Could also be a dictionary or array of multiple entry points
       entry: resolve(__dirname, 'src/await-invocation.ts'),
       name: 'await-invocation',
-      // the proper extensions will be added
-      fileName: 'await-invocation',
     },
-    rollupOptions: {
-      // make sure to externalize deps that shouldn't be bundled
-      // into your library
-      external: [],
-      output: {
-        // Provide global variables to use in the UMD build
-        // for externalized deps
-        globals: {},
-      },
-    },
+    sourcemap: true,
   },
 })
